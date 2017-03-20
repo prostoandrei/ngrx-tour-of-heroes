6e494a477fda99c115d445ff21bd3b3e3b7f8954
Vaganov Andrey <andrey.vaganov@aggregion.com>
2017-03-20 10:31:32 +0300

    litle changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,24 +20,6 @@ const HEROES: Hero[] = [
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-
+export class AppComponent {
   title = 'Tour of Heroes';
-  heroes: Hero[];
-  selectedHero: Hero;
-
-  constructor(
-    private heroService: HeroService
-  ) {}
-
-  ngOnInit() {
-    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    // this.heroes =
-
-    ;
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 }
