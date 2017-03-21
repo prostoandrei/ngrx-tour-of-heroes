c7af2e0e9fa6490fac7ec3b7393aa9e5fc8c8b49
prostoandrei <prostoandrei91@gmail.com>
2017-03-21 16:56:51 +0300

    added search component

--- src/app/components/hero-search/hero-search.component.ts
+++ src/app/components/hero-search/hero-search.component.ts
@@ -1,15 +1,43 @@
+import { Hero } from '../../models/hero';
+import { Router } from '@angular/router';
+import { Observable, Subject } from 'rxjs/Rx';
+import { HeroSearchService } from '../../services/hero-search.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-hero-search',
   templateUrl: './hero-search.component.html',
-  styleUrls: ['./hero-search.component.scss']
+  styleUrls: [ './hero-search.component.scss' ],
+  providers: [HeroSearchService]
 })
 export class HeroSearchComponent implements OnInit {
+  heroes: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(
+    private heroSearchService: HeroSearchService,
+    private router: Router) {}
 
-  ngOnInit() {
+  search(term: string): void {
+    this.searchTerms.next(term);
   }
 
+  ngOnInit(): void {
+    this.heroes = this.searchTerms
+      .debounceTime(300)        // wait 300ms after each keystroke before considering the term
+      .distinctUntilChanged()   // ignore if next search term is same as previous
+      .switchMap(term => term   // switch to new observable each time the term changes
+        // return the http search observable
+        ? this.heroSearchService.search(term)
+        // or the observable of empty heroes if there was no search term
+        : Observable.of<Hero[]>([]))
+      .catch(error => {
+        console.log(error);
+        return Observable.of<Hero[]>([]);
+      });
+  }
+  gotoDetail(hero: Hero): void {
+    const link = ['/detail', hero.id];
+    this.router.navigate(link);
+  }
 }
