c82ad7a8409f2183bd23dd9a1aef41c2c9aa7753
prostoandrei <prostoandrei91@gmail.com>
2017-04-07 08:59:14 +0300

    modified to present

--- src/app/effects/heroes.effects.ts
+++ src/app/effects/heroes.effects.ts
@@ -15,26 +15,37 @@ export class HeroesEffects {
     private store: Store<any>
   ) { }
 
-  // @Effect() getHeroes$ = this.actions$
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// @Effect() getHeroes$ = this.actions$
   //   .ofType(ActionTypes.GET_HEROES)
   //   .switchMap(() =>  // filter
   //     this.heroService.getHeroes() //
   //       .map(heroes => ({ type: ActionTypes.GET_HEROES_SUCCESS, payload: heroes }))
   //       .catch(() => Observable.of({type: ActionTypes.GET_HEROES_ERROR })));
 
-
-
-
-
-
-
-
   @Effect() createHero$ = this.actions$
     .ofType(ActionTypes.CREATE_HERO)
     .switchMap((action) =>
       this.heroService.create(action.payload)
         .map(hero => ({ type: ActionTypes.CREATE_HERO_SUCCESS, payload: hero }))
-        .catch(() => Observable.of({type: ActionTypes.GET_HEROES_ERROR })));
+        .catch(() => Observable.of({type: ActionTypes.CREATE_HERO_ERROR })));
 
   @Effect() deleteHero$ = this.actions$
     .ofType(ActionTypes.DELETE_HERO)
